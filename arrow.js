1698132fccb307e3539a2e0c0bf5a6bd4f2b5639
Victor <victor18ram@outlook.es>
2019-08-27 16:37:24 -0600

    arrow 6

---
+++ arrow.js
@@ -0,0 +1,11 @@
+var shape = {
+    name: "rectangle",
+    popup: function () {
+        console.log('This inside popup(): ' + this.name);
+        setTimeout(function () {
+            console.log('This insidesetTimeout(): ' + this.name);
+            console.log("I'm a " + this.name + "!");
+        }, 3000);
+    }
+};
+shape.popup();
