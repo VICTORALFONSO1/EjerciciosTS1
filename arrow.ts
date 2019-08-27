f4a013250874b44bc472e8f0dcc87efb87c3f270
Victor <victor18ram@outlook.es>
2019-08-27 16:41:26 -0600

    arrow sin I'am vacio 7

--- arrow.ts
+++ arrow.ts
@@ -4,7 +4,7 @@ var shape = {
 
         console.log('This inside popup(): ' + this.name);
 
-        setTimeout(function(){
+        setTimeout( () => {
             console.log('This insidesetTimeout(): ' + this.name);
             console.log("I'm a " + this.name + "!");
         }, 3000);
