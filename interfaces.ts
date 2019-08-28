a014cbd1f10f328a76859a062aea3d92a23464ce
Victor <victor18ram@outlook.es>
2019-08-28 10:33:11 -0600

    interfaces signando valores 26

--- interfaces.ts
+++ interfaces.ts
@@ -3,6 +3,15 @@ interface Lakes {
     area: number,
     length: number,
     depth: number,
-    isFreshWater: boolean,
+    isFreshwater: boolean,
     countries: string[]
+}
+
+let firstLake: Lakes = {
+    name: 'Caspian Sea',
+    length: 1199,
+    depth: 1025,
+    area: 371000,
+    isFreshwater: false,
+    countries: ['Kazakhstan', 'Russia', 'Turkmenistan', 'Azerbaijan', 'Iran']
 }
